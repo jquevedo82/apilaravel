5017c82b1f11f13597a999d593ab810e1db6c2ef
jquevedo82 <93448578+jquevedo82@users.noreply.github.com>
2025-04-29 16:23:38 -0300

    []

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        User::factory()->create([
+            'name' => config(key: 'blog.admin_name'),
+            'email' => config(key: 'blog.admin_email'),
+            'password' => bcrypt(config(key: 'blog.admin_password')),
+           // 'is_admin' => true,
+         ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
